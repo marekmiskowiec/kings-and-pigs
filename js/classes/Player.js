2d32b1de6706dd019363a9a095dbe3e9b34ebd39
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2025-01-10 10:59:44 +0100

    add gravity

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,6 +4,12 @@ class Player {
       x: 100,
       y: 100,
     };
+
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
     this.width = 100;
     this.height = 100;
 
@@ -11,6 +17,8 @@ class Player {
     this.sides = {
       bottom: this.position.y + this.height,
     };
+
+    this.gravity = 1;
   }
 
   draw() {
@@ -19,9 +27,12 @@ class Player {
   }
 
   update() {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y++;
+    this.position.y += this.velocity.y;
+
+    // above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity;
       this.sides.bottom = this.position.y + this.height;
-    }
+    } else this.velocity.y = 0;
   }
 }
